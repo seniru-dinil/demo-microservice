c004cedb45ed906e6229d13efa0cede396052c5b
seniru-dinil <senirudinil927@gmail.com>
2025-05-14 10:22:59 +0530

    handle department not found exception done

--- employee-service/src/main/java/edu/icet/employee/service/EmployeeService.java
+++ employee-service/src/main/java/edu/icet/employee/service/EmployeeService.java
@@ -7,32 +7,33 @@ import edu.icet.employee.client.DepartmentClient;
 import edu.icet.employee.dto.EmployeeDTO;
 import edu.icet.employee.entity.EmployeeEntity;
 import edu.icet.employee.repository.EmployeeRepo;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class EmployeeService {
 
 private final EmployeeRepo employeeRepo;
-private final RestTemplate restTemplate;
 private final DepartmentClient departmentClient;
 
     public EmployeeDTO create(EmployeeDTO employee){
         Integer departmentId = employee.getDepartmentId();
         try{
-            ResponseEntity<DepartmentDTO> byId = departmentClient.getById(departmentId);
             return mapToDTO(employeeRepo.save(
                     EmployeeEntity.builder()
                             .name(employee.getName())
-                            .departmentId(employee.getDepartmentId())
+                            .departmentId(Objects.requireNonNull(departmentClient.getById(departmentId).getBody()).getId())
                             .address(employee.getAddress())
                             .build()
             ));
-        }catch (Exception e){
-             throw new DepartmentNotFoundException(e.getMessage());
+        }catch (FeignException e){
+             throw new DepartmentNotFoundException("INVALID ID");
         }
     }
 
